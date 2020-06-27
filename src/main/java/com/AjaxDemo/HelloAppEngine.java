65a4f325966c729cb9e1cacc0b17a84349c0cc48
Sivaskandha Roopan <sivaskandharuban@gmail.com>
2020-06-27 21:41:16 +0530

    HTML,Ajax,Javascript and JSON practice

---
+++ src/main/java/com/AjaxDemo/HelloAppEngine.java
@@ -0,0 +1,36 @@
+package com.AjaxDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+    
+    PrintWriter out = response.getWriter();
+    
+    out.print("Hello world");
+    
+    String message = request.getParameter("message");
+    System.out.println("entered message " + message);
+    
+    
+
+    
+
+  }
+}
